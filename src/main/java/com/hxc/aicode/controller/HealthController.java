3cb822e56f62a9d8a8ad515d34c80ccdb678274e
hxchen <18813043577@163.com>
2025-07-29 16:31:30 +0800

    初始化项目结构及基础功能

---
+++ src/main/java/com/hxc/aicode/controller/HealthController.java
@@ -0,0 +1,21 @@
+package com.hxc.aicode.controller;
+
+import com.hxc.aicode.common.BaseResponse;
+import com.hxc.aicode.common.ResultUtils;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping("/")
+    public BaseResponse<String> healthCheck() {
+        return ResultUtils.success( "ok");
+    }
+}
+
+
+
